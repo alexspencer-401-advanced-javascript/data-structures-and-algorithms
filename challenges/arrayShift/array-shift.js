f4c2d82b508133d7aafc94082c164f30d7fe46ec
Alex Spencer <alexander.j.spencer@gmail.com>
2019-09-17 10:28:11 -0700

    solved code challenge two

--- challenges/arrayShift/array-shift.js
+++ challenges/arrayShift/array-shift.js
@@ -1,6 +1,14 @@
-const arr = [2,4,6,8];
-const value = 5;
-
-function insertShiftArray() {
-  
+function insertShiftArray(input, value) {
+  const middle = ((input.length) / 2);
+  const newArr = [];
+  for(let i = 0; i < middle; i++) {
+    newArr[newArr.length] = input[i];
+  }
+  newArr[newArr.length] = value;
+  for(let i = middle; i < input.length; i++) {
+    newArr[newArr.length] = input[i];
+  }
+  return newArr;
 }
+
+module.exports = insertShiftArray;

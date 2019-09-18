9c2aacf27b958594cc4af77abb52a8371c8d2921
Alex Spencer <alexander.j.spencer@gmail.com>
2019-09-18 13:58:28 -0700

    think I completed it

--- challenges/arrayBinarySearch/array-binary-search.js
+++ challenges/arrayBinarySearch/array-binary-search.js
@@ -1,10 +1,22 @@
 function binarySearch(arr, key) {
-  for(let i = 0; i < arr.length; i++) {
-    const number = arr[i];
-    if(number === key) {
-      return i;
+  let startIndex = 0;
+  let endIndex = arr.length - 1;
+  
+  while(startIndex <= endIndex) {
+    let middleIndex = Math.floor((startIndex + endIndex) / 2);
+
+    if(key === arr[middleIndex]) {
+      return middleIndex;
     }
-  } 
+    if(key > arr[middleIndex]) {
+      startIndex = middleIndex += 1;
+    } else if(key < arr[middleIndex]) {
+      endIndex = middleIndex -= 1;
+    } 
+  }
+  return -1;
 }
 
 module.exports = binarySearch;
+
+// Found this to be a very helpful resource https://medium.com/@jeffrey.allen.lewis/javascript-algorithms-explained-binary-search-25064b896470

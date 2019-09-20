1f54846f4a73d35e3e519e8639d22b38ad75ac7a
Alex Spencer <alexander.j.spencer@gmail.com>
2019-09-20 12:29:25 -0700

    battery running low, just commiting now

---
+++ challenges/linkedList/linked-list.js
@@ -0,0 +1,45 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+  }
+  
+  insert(value) {
+    const newNode = new Node(value);
+    this.head = newNode; 
+    newNode.next = this.head;
+    this.size++; 
+  }
+  
+  includes(value) {
+    let currentNode = this.head;
+    for(let i = 0; i < this.size; i++ ) {
+      if(currentNode.value === value) {
+        return true;
+      } else {
+        currentNode = currentNode.next;
+      }
+    } return false;
+  }
+
+  toString() {
+    let string;
+    let currentNode = this.head;
+    for(let i = 0; i < this.size; i++ ) {
+      string = string + currentNode.value;
+      currentNode = currentNode.next;
+    }
+    return string;
+  }
+}
+
+module.exports = {
+  Node,
+  LinkedList
+};
---
+++ tests/linked-list.test.js
@@ -0,0 +1,5 @@
+const linkedList = require('../challenges/linkedList/linked-list');
+
+describe('it does something with linked lists', () => {
+  it('does something here', () => {
+}

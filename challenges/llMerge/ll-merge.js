c8688e62583b4921635f6b745625e5f35db92e82
Alex Spencer <alexander.j.spencer@gmail.com>
2019-09-24 16:36:49 -0700

    init and built test, now function

---
+++ challenges/llMerge/ll-merge.js
@@ -0,0 +1,5 @@
+function mergeLists() {
+
+}
+
+module.exports = mergeLists;
---
+++ tests/ll-merge.test.js
@@ -0,0 +1,18 @@
+const linkListImport = require('../challenges/linkedList/linked-list');
+const mergeLists = require('../challenges/llMerge/ll-merge');
+
+describe('Linked Lists Merge', () => {
+  
+  it('merges to linked lists in zipper fashion', () => {
+    const list1 = new linkListImport.LinkedList();
+    const list2 = new linkListImport.LinkedList();
+    list1.insert('1');
+    list1.insert('3');
+    list1.insert('5');
+    list2.insert('2');
+    list2.insert('4');
+    list2.insert('6');
+    const result = mergeLists(list1, list2);
+    expect(result).toBe('1', '2', '3', '4', '5', '6');
+  });
+});

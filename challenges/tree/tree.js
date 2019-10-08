c2f3e2b84898e25bf2348901772e719642e8b0a8
Alex Spencer <alexander.j.spencer@gmail.com>
2019-10-08 14:57:58 -0700

    completed preorder

--- challenges/tree/tree.js
+++ challenges/tree/tree.js
@@ -13,13 +13,23 @@ class BinaryTree {
   }
 
   preOrder(node) {
-    let newArr = [];
-    if(node !== null) {
-      newArr.push(node.value);
-      this.preOrder(node.left);
-      this.preOrder(node.right);
+    if(node === null) return [];
+
+    let stack = [];
+    let result = [];
+
+    stack.push(node);
+
+    while(stack.length > 0) {
+      let current = stack.pop();
+      result.push(current.value);
+      if(current.right) stack.push(current.right);
+      if(current.left) stack.push(current.left);
     }
+    return result;
+
   }
+
   inOrder(node) {
     let newArr = [];
     if(node !== null) {
--- tests/tree.test.js
+++ tests/tree.test.js
@@ -7,18 +7,47 @@ describe('tree testing', () => {
     expect(tree.root).toBe(null);
   });
 
-  it('can successfully make a tree', () => {
+  it('can successfully make a tree with single root', () => {
+    const tree = new BinaryTree();
+    tree.root = new Node('root');
+    expect(tree.root.value).toBe('root');
+  });
+  it('can successfully make a tree with right and left child', () => {
+    const tree = new BinaryTree();
+    tree.root = new Node('root');
+    tree.root.left = new Node('left');
+    tree.root.right = new Node('right');
+    expect(tree.root.value).toBe('root');
+    expect(tree.root.left.value).toBe('left');
+    expect(tree.root.right.value).toBe('right');
+  });
+
+  it('can successfully return a collection from a preorder traversal', () => {
     const tree = new BinaryTree();
     tree.root = new Node('F');
     tree.root.left = new Node('B');
     tree.root.left.left = new Node('A');
     tree.root.left.right = new Node('D');
-    tree.root.left.left.left = new Node('C');
+    tree.root.left.right.left = new Node('C');
     tree.root.left.right.right = new Node('E');
     tree.root.right = new Node('G');
     tree.root.right.right = new Node('I');
     tree.root.right.right.left = new Node('H');
-    // console.log(tree);
-    // expect(tree).toEqual({});
+    expect(tree.preOrder(tree.root)).toEqual(['F', 'B', 'A', 'D', 'C', 'E', 'G', 'I', 'H']);
   });
+
+  it('can successfully return a collection from an inorder traversal', () => {
+    const tree = new BinaryTree();
+    tree.root = new Node('F');
+    tree.root.left = new Node('B');
+    tree.root.left.left = new Node('A');
+    tree.root.left.right = new Node('D');
+    tree.root.left.right.left = new Node('C');
+    tree.root.left.right.right = new Node('E');
+    tree.root.right = new Node('G');
+    tree.root.right.right = new Node('I');
+    tree.root.right.right.left = new Node('H');
+    expect().toEqual(['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I']);
+  });
+
 });
